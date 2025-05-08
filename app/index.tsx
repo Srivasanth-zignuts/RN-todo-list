7386088fda7ec3d6e5fc4846ce33dcd3d0943704
Srivasanth-zignuts <srivasanthj@zignuts.com>
2025-05-08 18:05:09 +0530

    setup done

---
+++ app/index.tsx
@@ -0,0 +1,58 @@
+import { FlatList, Text, View } from 'react-native';
+
+export default function Index() {
+	const todoData = [
+		{
+			id: 1,
+			title: 'Todo 1',
+			isDone: false,
+		},
+		{
+			id: 2,
+			title: 'Todo 2',
+			isDone: false,
+		},
+		{
+			id: 3,
+			title: 'Todo 3',
+			isDone: false,
+		},
+		{
+			id: 4,
+			title: 'Todo 4',
+			isDone: true,
+		},
+		{
+			id: 5,
+			title: 'Todo 5',
+			isDone: false,
+		},
+		{
+			id: 6,
+			title: 'Todo 6',
+			isDone: false,
+		},
+	];
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<FlatList
+				data={todoData}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => {
+					return (
+						<View>
+							<Text>{item.title}</Text>
+						</View>
+					);
+				}}
+			/>
+		</View>
+	);
+}
